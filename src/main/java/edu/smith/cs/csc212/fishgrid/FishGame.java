3e938f374b14790985f376ddf16ad1ec9b291e07
Meredith12 <Meredith12@131.229.237.9>
2019-11-25 12:45:42 -0500

    Made 2D arrays and initialized nums and booleans

--- src/main/java/edu/smith/cs/csc212/fishgrid/FishGame.java
+++ src/main/java/edu/smith/cs/csc212/fishgrid/FishGame.java
@@ -23,7 +23,14 @@ public class FishGame {
 	 * Score!
 	 */
 	int score;
-	
+	/**
+	 * 2D Array of booleans to hold mines
+	 */
+	boolean[][] mines;
+	/**
+	 * 2D Array of ints to determine how many mines coordinate touches
+	 */
+	int[][]mineNum;
 	/**
 	 * Create a FishGame of a particular size.
 	 * @param w how wide is the grid?
@@ -31,11 +38,13 @@ public class FishGame {
 	 */
 	public FishGame(int w, int h) {
 		world = new World(w, h);
-		
+		mines = new boolean[14][14];
+		mineNum = new int[14][14];
+		initializeMines();
 		// Make the player out of the 0th fish color.
 		player = new Fish(0, world);
 		// Start the player at "home".
-		player.setPosition(6, 6);
+		player.setPosition(7, 7);
 		player.markAsPlayer();
 		world.register(player);
 		
@@ -70,6 +79,70 @@ public class FishGame {
 		List<WorldObject> atPoint = world.find(x, y);
 		
 
+	}
+	
+	public boolean isValidIndex(int x, int y) {
+		if(x<0||x>13||y<0||y>13)
+			return false;
+		return true;
+	}
+	
+	/**
+	 * Fills 2D arrays with random mines and the number of mines surrounding
+	 */
+	public void initializeMines() {
+		//Fills mines
+		int mineCount=0;
+		while(mineCount<40) {
+			int x = (int) (Math.random()*14);
+			int y = (int)(Math.random()*14);
+			if(mines[x][y]==false) {
+				mines[x][y]=true;
+				mineCount++;
+			}
+		}
+		//Fills mineNum
+		for(int x=0; x<14;x++) {
+			for(int y=0; y<14;y++) {
+				int count=0;
+				if(isValidIndex(x-1,y-1)&&mines[x-1][y-1]==true)
+					count++;
+				if(isValidIndex(x,y-1)&&mines[x][y-1]==true)
+					count++;
+				if(isValidIndex(x+1,y-1)&&mines[x+1][y-1]==true)
+					count++;
+				if(isValidIndex(x-1,y)&&mines[x-1][y]==true)
+					count++;
+				if(isValidIndex(x+1,y)&&mines[x+1][y]==true)
+					count++;
+				if(isValidIndex(x-1,y+1)&&mines[x-1][y+1]==true)
+					count++;
+				if(isValidIndex(x,y+1)&&mines[x][y+1]==true)
+					count++;
+				if(isValidIndex(x+1,y+1)&&mines[x+1][y+1]==true)
+					count++;
+				mineNum[x][y]=count;
+			}
+		}
+		for(int x=0; x<14;x++) {
+			for(int y=0; y<14;y++) {
+				if(mines[x][y]==true)
+					System.out.print("1");
+				else
+					System.out.print("0");
+				System.out.print(" ");
+			}
+			System.out.println();
+		}
+		System.out.println();
+		for(int x=0; x<14;x++) {
+			for(int y=0; y<14;y++) {
+				System.out.print(mineNum[x][y]+" ");
+			}
+			System.out.println();
+		}
+		
+		
 	}
 	
 }
